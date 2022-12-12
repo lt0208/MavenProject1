a97c4e18d0a25a31ff0fcae81c86a61a2e5023ae
lt0208 <lt02080208@gmail.com>
2022-12-11 20:44:36 -0800

    first

---
+++ src/main/java/MavenJenkins.java
@@ -0,0 +1,8 @@
+import java.util.logging.Logger;
+
+public class MavenJenkins {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(MavenJenkins.class.getName());
+        logger.info("main class...");
+    }
+}
---
+++ src/test/java/MavenJenkinsTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MavenJenkinsTest {
+    @Test
+    public void testDemo(){
+        Logger logger = Logger.getLogger(MavenJenkins.class.getName());
+        logger.info("testing...");
+        assertTrue(true);
+    }
+
+}
